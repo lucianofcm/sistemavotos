5b798095401818274b9fac7e96e912204a547066
lucianofcm <lucweb@gmail.com>
2021-08-18 12:48:33 -0300

    first commit

---
+++ src/main/java/com/sistemavotos/SistemavotosApplication.java
@@ -0,0 +1,13 @@
+package com.sistemavotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemavotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemavotos/SistemavotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemavotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemavotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
