56d30f65793fbae2c6efff4a943b8b23054ba3b6
lucianofcm <lucweb@gmail.com>
2021-08-24 02:48:05 -0300

    Configuração swagger

--- src/main/java/com/sistemavotos/SistemavotosApplication.java
+++ src/main/java/com/sistemavotos/SistemavotosApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
 @SpringBootApplication
 @ComponentScan("com.sistemavotos.*")
 @EnableRabbit
+@EnableSwagger2
 public class SistemavotosApplication {
 
 	public static void main(String[] args) {
